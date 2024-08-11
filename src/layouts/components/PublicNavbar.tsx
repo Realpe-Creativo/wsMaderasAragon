edb4b22ff74ffb176fffc907f0c9b7026af13216
Zadiaz <ed.diazf@gmail.com>
2024-08-11 18:25:52 -0500

    Update PublicNavbar.tsx

--- src/layouts/components/PublicNavbar.tsx
+++ src/layouts/components/PublicNavbar.tsx
@@ -44,13 +44,13 @@ export default function  (props:props) {
           </div>
         </div>
           <div className="flex bg-stone-900/65  flex-row">
-            <div className="flex w-3/12 ms-16">
+            <div className="flex w-2/12 md:w-3/12 ms-4 md:ms-16">
               <Link onClick={()=> setTimeout(()=>scrollTo({ y: 0 }),300)} to="/" className="flex w-full  items-center align-middle">
                 <img src="/logo-white.png" className="w-14 mx-2 flex" alt="Logo Maderas Aragón" />
                 <span className="hidden md:flex text-2xl font-title-bold text-white leading-5">Maderas Aragón</span>
               </Link>
             </div>
-            <div className="flex w-6/12 p-2 justify-center">
+            <div className="flex w-7/12 md:w-6/12 p-2 justify-center">
               <Link onClick={()=> setTimeout(()=>scrollTo({ y: 0 }),300)} to="/products" className={`flex bg-stone-600 h-fit transition-all px-2 items-center align-middle rounded-3xl justify-center py-1 hover:drop-shadow-md hover:bg-stone-800 text-stone-200 hover:text-white mx-2 ${props.tab === 'products'?'bg-stone-800/75 text-white':''}`}>
                 <LuShoppingBag className="flex m-1" />
                 <span className="md:flex hidden">Productos</span>
@@ -66,7 +66,7 @@ export default function  (props:props) {
                 <span className="md:flex hidden">Conócenos</span>
               </Link>
             </div>
-            <div className="flex w-3/12 ms-12">
+            <div className="flex w-1/12 md:w-3/12 ms-12">
             </div>
           </div>
       </div>
