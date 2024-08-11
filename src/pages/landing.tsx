b258fe8288ca9f33426d6e25eeab84647c230409
Zadiaz <ed.diazf@gmail.com>
2024-08-11 16:57:48 -0500

    Update landing.tsx

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -14,7 +14,7 @@ export default function  (props?: LoadingProps) {
     <>
     <div className="relative">
       <video src="/banner.mp4" muted autoPlay loop className="absolute hero-video z-0"></video>
-      <div className="w-screen  mt-4 flex justify-center items-center align-middle h-screen bg-stone-900 bg-fixed p-6">
+      <div className="w-[100vw]  mt-4 flex justify-center items-center align-middle h-[100vh] bg-stone-900 bg-fixed p-6">
         <div
           onMouseEnter={() => setHovered(true)}
           onMouseLeave={() => setHovered(false)}
